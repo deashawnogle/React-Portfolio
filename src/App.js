8b031ed6ce1ea1d916083b15b56aed07f007beca
deashawnogle <deashawnogle@gmail.com>
2022-03-13 05:25:22 -0700

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-import BucketList from './components/BucketList';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Main from './components/Main';
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+import Wrapper from './components/Wrapper';
 
-function App() {
-  return (
-    <div className="bucket-app">
-      <BucketList />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <Wrapper>
+        <Swtich>
+          <Route exact path="/" Component={Main} />
+          <Route exact path="/project" Component={Projects} />
+          <Route exact path="/contact" Component={Contact} />
+        </Swtich>
+      </Wrapper>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import BucketForm from './BucketForm';
+import BucketForm from './Projects';
 
 function Bucket(props) {
   const [edit, setEdit] = useState({
